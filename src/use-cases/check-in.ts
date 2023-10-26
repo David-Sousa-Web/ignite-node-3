ef04b3ae7c67ea7ddc1bf43ea904ce65d6bd4622
David Sousa <david01sousa@hotmail.com>
2023-10-25 21:36:19 -0300

    feat: gym repository

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,20 +1,35 @@
 import { CheckIn} from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLogintude: number
 }
 interface CheckInUseCaseResponse {
   checkIn: CheckIn
 }
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({ 
     userId, 
     gymId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findbyId(gymId)
+
+    if(!gym) {
+      throw new ResourceNotFoundError
+    }
+
+    //calculate distance between user and gym
+    
     const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
