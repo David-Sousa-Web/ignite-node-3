d25a563750daf8a07316aae5208524f62b1e75b6
David Sousa <david01sousa@hotmail.com>
2024-01-30 10:45:16 -0300

    feat: prisma user repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,25 +1,31 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma} from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { UserRepository } from "../users-repository";
 
 export class PrismaUserRepository implements UserRepository {
-    findbyId(id: string): Promise<{ id: string; name: string; email: string; password_hash: string; created_at: Date; } | null> {
-        throw new Error("Method not implemented.");
-    }
-    async findByEmail(email: string) {
-        const user = await prisma.user.findUnique({
-            where: {
-                email,
-            }
-        })
+  async findbyId(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-        return user
-    }
-    async create(data: Prisma.UserCreateInput) {
-        const user = await prisma.user.create({
-            data,
-        })
+    return user;
+  }
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        return user
-    }
+    return user;
+  }
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
 }
