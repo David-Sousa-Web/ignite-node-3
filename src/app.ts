932dcbb19796744a442e28aaab353248b96f8144
David Sousa <david01sousa@hotmail.com>
2024-01-30 19:23:43 -0300

    feat: jwt-secret setup

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,30 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 
-app.register(appRoutes)
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
+app.register(appRoutes);
 
-app.setErrorHandler((error, _request, reply)=> {
+app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
-      message: 'validation error', issues: error.format()
-    })
+      message: "validation error",
+      issues: error.format(),
+    });
   }
 
-  if(env.NODE_ENV !== "production") {
-    console.error(error)
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   } else {
     // TODO: Here we should log to an external tool like DataDog/newRelic/Sentry
   }
 
   return reply.status(500).send({
-    message: 'internal server error'
-  })
-})
+    message: "internal server error",
+  });
+});
