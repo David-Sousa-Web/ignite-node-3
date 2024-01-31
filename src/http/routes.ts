db50d316dcc73f2758c6a39b2fd61cc2e205b494
David Sousa <david01sousa@hotmail.com>
2024-01-30 21:10:30 -0300

    feat: auth rotes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { VerifyJwt } from "./middlewares/verify-jwt";
+import { profile } from "./controllers/profile";
 
 export async function appRoutes(app: FastifyInstance) {
-    app.post('/users', register)
-    app.post('/sessions', authenticate)
+  app.post("/users", register);
+  app.post("/sessions", authenticate);
+
+  //auth
+  app.get("/me", { onRequest: [VerifyJwt] }, profile);
 }
